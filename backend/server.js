13711b64ae00ead2b26a6cc680344a1ff703f02b
usUpal <udyan.upal@northsouth.edu>
2021-05-21 21:46:47 +0600

    server.js

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,17 @@ app.use(express.json());
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
+
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
-app.listen(()=> {
+const exercisesRouter = require('./routes/exercises')
+const usersRouter = require('./routes/users')
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, ()=> {
     console.log(`server: ${port}`);
 });
